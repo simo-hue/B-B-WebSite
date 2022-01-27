cd7d2efbd872ad16f2368a7b52a035da48da5050
Mattioli Simone <mattioli.simone.10@gmail.com>
2022-01-27 20:10:40 +0100

    implementazione termini e condizioni

--- layout/scripts/alterBtnInvia.js
+++ layout/scripts/alterBtnInvia.js
@@ -1,3 +1,21 @@
-function cliccato() {
-    alert("btn cliccato (debugging");
+(async () => {
+
+  const { value: accept } = await Swal.fire({
+    title: 'Termini e Condizioni',
+    input: 'checkbox',
+    inputValue: 1,
+    inputPlaceholder:
+      'Accetto I Termini E Le Condizioni',
+    confirmButtonText:
+      'Continue <i class="fa fa-arrow-right"></i>',
+    inputValidator: (result) => {
+      return !result && 'Per continuare sul nostro sito devi Accettare Le Nostre condizioni'
+    }
+  })
+  
+  if (accept) {
+    Swal.fire('Grazie Per Aver Deciso Di Accettare I Termini E Le Nostre Condizioni')
   }
+  
+  
+  })()
---
+++ layout/scripts/animazioneSweetAlert2.js
@@ -0,0 +1,10 @@
+const { value: email } = await Swal.fire({
+    title: 'Input email address',
+    input: 'email',
+    inputLabel: 'Your email address',
+    inputPlaceholder: 'Enter your email address'
+  })
+  
+  if (email) {
+    Swal.fire(`Entered email: ${email}`)
+  }
